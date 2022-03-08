c83032d675ba9bfeb1e0301b80a7aa163bade113
ahmedwal8d <85419960+ahmedwal8d@users.noreply.github.com>
2022-03-08 04:21:36 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 let SkillsSection = document.querySelector(".our-skill")
 let Header = document.querySelector(".header")
 let ButtonToTop = document.querySelector("span.up")
+let Headerlink = document.querySelector(".header ul li.other")
+let HeaderDropdowun = document.querySelector(".header ul li.other .link-content")
+let ButtonDarkMood = document.querySelector(".header .dark-mood")
 
 window.onscroll = function () {
     let SkillsSectionOffset = SkillsSection.offsetTop;
@@ -50,3 +53,42 @@ ButtonToTop.onclick = function () {
         behavior: "smooth"
     })
 }
+
+Headerlink.onclick = function () {
+    HeaderDropdowun.classList.toggle("active")
+}
+
+ButtonDarkMood.onclick = function () {
+    document.body.classList.toggle("dark-items")
+}
+
+let StateSection = document.querySelector(".state")
+let StateElement = document.querySelectorAll(".state span");
+let Started = false;
+console.log(StateElement[0])
+
+window.onscroll = function () {
+    let StateSatateOffset = StateSection.offsetTop;
+
+    if (window.scrollY > StateSatateOffset) {
+
+        if (!Started) {
+            StateElement.forEach((num)=> {
+                StartCounte(num)
+            })
+        }
+
+        Started = true;
+    }
+}
+
+function StartCounte(el) {
+    let goal = el.dataset.goal;
+    let counte = setInterval(function () {
+        el.textContent++;
+        if (el.textContent === goal) {
+            clearInterval(counte)
+        }
+    }, 2000 / goal)
+}
+
