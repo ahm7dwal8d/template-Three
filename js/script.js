88b3a7e079cca1260e998ed7784caa32a54a7504
ahmedwal8d <85419960+ahmedwal8d@users.noreply.github.com>
2022-04-15 03:18:00 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -1,134 +1,237 @@
+// Header Section 
+
+$(".header ul li > a").click(function (event) {
+
+    event.preventDefault()
+
+    $(this).addClass("active").parent().siblings().find("a").removeClass("active")
+
+    console.log()
+
+    $("html , body").animate({
+
+        scrollTop: $("#" + $(this).data("scroll")).offset().top - $(".header").innerHeight()
+
+    })
+
+})
+
+
+$(window).scroll(function () {
+
+    if ($(window).scrollTop() >= 800) {
+
+        $(".header").addClass("fixed")
+        
+    } else {
+
+
+        $(".header").removeClass("fixed")
+
+    }
+
+})
+
+// landing Section 
+
+$(".landing .down-section a").click(function () {
+
+    console.log()
+
+    $("html , body").animate({
+
+        scrollTop: $("#" + $(this).data("start")).offset().top - $(".header").innerHeight() - 60
+
+    }) 
+
+})
+
+// Gallery Section 
+
+let GallerySection = document.querySelector(".gallery")
+
+let GalleryImg = document.querySelectorAll(".gallery img")
+
+GalleryImg.forEach((img)=> {
+
+    img.addEventListener("click" , function () {
+
+        let Overly = document.createElement("div")
+
+        Overly.className = "overly-box"
+
+        document.body.appendChild(Overly)
+
+        let BoxImg = document.createElement("div")
+
+        BoxImg.className = "box-img"
+
+        Overly.appendChild(BoxImg)
+
+        let ImgBox = document.createElement("img")
+
+        ImgBox.src = img.src
+
+        BoxImg.appendChild(ImgBox)
+
+        let CloseButton = document.createElement("span")
+
+        CloseButton.className = "close-button"
+
+        BoxImg.appendChild(CloseButton)
+
+        CloseButtonText = document.createTextNode("X")
+
+        CloseButton.appendChild(CloseButtonText)
+
+        document.addEventListener("click" , function (el) {
+
+            if (el.target.className === "close-button") {
+
+                BoxImg.remove()
+
+                Overly.remove()
+
+            }
+
+        })
+
+    })
+
+})
+
 let SkillsSection = document.querySelector(".our-skill")
+
 let Header = document.querySelector(".header")
+
 let ButtonToTop = document.querySelector("span.up")
-let Headerlink = document.querySelector(".header ul li.other")
-let HeaderDropdowun = document.querySelector(".header ul li.other .link-content")
-let ButtonDarkMood = document.querySelector(".header .dark-mood")
+
+// Date Section 
+
+// let CounteDate = new Date().getTime()
+
+// console.log(CounteDate)
+
+// State Section 
+
+let StateSection = document.querySelector(".state")
+
+let StateOffsetTop = StateSection.offsetTop
+
+let StateNum = document.querySelectorAll(".state p")
+
+let Started = false;
+
+function StartCounter(el) {
+
+    let goal = el.dataset.goal;
+
+    let Counte = setInterval(function () {
+
+        el.textContent++;
+
+        if (el.textContent === goal) {
+
+            clearInterval(Counte)
+
+        }
+    } , 2000 / goal )
+
+}
 
 window.onscroll = function () {
+
+    // Skills Section 
+
     let SkillsSectionOffset = SkillsSection.offsetTop;
+
     let SkillsSectionHeight = SkillsSection.offsetHeight;
+
     let PageHeight = this.innerHeight;
+
 
     if (window.scrollY > (SkillsSectionOffset + SkillsSectionHeight - PageHeight)) {
 
         let SkillsDiv = document.querySelectorAll(".our-skill .prog span")
 
         SkillsDiv.forEach((skills) => {
+
             skills.style.width = skills.dataset.progres
         })
         
     }
 
-    if (window.scrollY < (SkillsSectionOffset + SkillsSectionHeight - PageHeight)) {
+    // testmonlies Section 
 
-        let SkillsDiv = document.querySelectorAll(".our-skill .prog span")
+    let far = document.querySelectorAll(".testimonials .rate .far")
 
-        SkillsDiv.forEach((skills) => {
-            skills.style.width = "0"
+    far.forEach((i)=> {
+
+        i.addEventListener("click" , function () {
+
+            i.classList.add("active")
+
         })
-    }
 
-    
-    let StateSatateOffset = StateSection.offsetTop;
+    })
 
-    if (window.scrollY > StateSatateOffset) {
+    // Video Section 
+
+    $(".video .container .video-content .info li").click(function () {
+
+        $(this).addClass("active").siblings().removeClass("active")
+
+        // var ImgHide = $(this).data("hide")
+
+        // $(".video .container .video-content .images .img").fadeOut()
+        // $(".video .container .video-content .images ." + ImgHide ).fadeIn()
+
+    })
+
+
+
+    // State Section 
+
+    if (window.scrollY >= StateOffsetTop) {
 
         if (!Started) {
-            StateElement.forEach((num)=> {
-                StartCounte(num)
+
+            StateNum.forEach((num)=> {
+
+                StartCounter(num)
+
             })
+
         }
+        
+        Started = true
 
-        Started = true;
-    }
-
-// Fixed Header
-    if (window.scrollY >= 800) {
-        Header.classList.add("fixed")
-    } else {
-        Header.classList.remove("fixed")
-    }
-
-    // ButtonToTop Active
-    if (window.scrollY >= 800) {
-        ButtonToTop.classList.add("active")
-    } else {
-        ButtonToTop.classList.remove("active")
     }
 }
 
-// ButtonToTop Click 
+// Button To Top 
 
-ButtonToTop.onclick = function () {
-    window.scrollTo({
-        left: 0,
-        top: 0,
-        behavior: "smooth"
+$("span.up").click(function () {
+
+    $("html , body").animate({
+
+        scrollTop: 0
+
     })
-}
 
-Headerlink.onclick = function () {
-    HeaderDropdowun.classList.toggle("active")
-}
+})
 
-ButtonDarkMood.onclick = function () {
-    document.body.classList.toggle("dark-items")
-}
+$(window).scroll(function () {
 
-let StateSection = document.querySelector(".state")
-let StateElement = document.querySelectorAll(".state span");
-let Started = false;
-console.log(StateElement[0])
+    if ($(window).scrollTop() >= 800) {
+
+        $("span.up").addClass("active")
+        
+    } else {
 
 
-function StartCounte(el) {
-    let goal = el.dataset.goal;
-    let counte = setInterval(function () {
-        el.textContent++;
-        if (el.textContent === goal) {
-            clearInterval(counte)
-        }
-    }, 2000 / goal)
-}
+        $("span.up").removeClass("active")
 
-// Gallery Section 
-let galleryimg = document.querySelectorAll(".gallery .image img");
-console.log(galleryimg[0])
+    }
 
-galleryimg.forEach((img) => {
-    img.addEventListener("click" , function() {
-        let overly = document.createElement("div")
-        overly.className = "popup-overly"
-        document.body.appendChild(overly)
-        let PopupBox = document.createElement("div")
-        PopupBox.className = "popup-box"
-        if (img.alt !== null) {
-            let TextHead = document.createElement("h3")
-
-            let Text = document.createTextNode(img.alt)
-
-            TextHead.appendChild(Text)
-
-            PopupBox.appendChild(TextHead)
-        }
-        let PopupImg = document.createElement("img")
-        PopupImg.src = img.src;
-        PopupBox.appendChild(PopupImg)
-        document.body.appendChild(PopupBox)
-
-        let CloseButton = document.createElement("span");
-        let CloseButtonText = document.createTextNode("X")
-        CloseButton.appendChild(CloseButtonText)
-        CloseButton.className = "close-button"
-        PopupBox.appendChild(CloseButton)
-
-        // Remove Popup From Dom 
-        document.addEventListener("click" , function (e) {
-            if (e.target.className === "close-button") {
-                e.target.parentNode.remove()
-                document.querySelector(".popup-overly").remove()
-            }
-        })
-    })
 })
